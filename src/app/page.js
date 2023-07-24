30557f50b5b3d0f9a3aabba195f6a044318fddfc
David Auk <auk.david@gmail.com>
2023-07-24 09:32:53 +0200

    Revert "Update text"
    This reverts commit aa8168d020ff6fb2d9e097edf15634abcf6fe7d8.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Hello world!
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
           <a
